300d68f50e9f76a266b686ffbf1e20e0e68e30e2
Vladimir Nybozhinsky <vladimirnybozhinsky@Mac-mini-Vladimir.local>
2018-03-11 14:51:25 +0200

    create

--- Package.swift
+++ Package.swift
@@ -11,13 +11,19 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-    ],
+        .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.0.0")),
+        ],
     targets: [
-        .target(
-            name: "App",
-            dependencies: ["Vapor", "FluentProvider"],
-            exclude: ["Config", "Public", "Resources"]
-        ),
+        .target(name: "App", dependencies: ["Vapor",
+                                            "FluentProvider",
+                                            "ValidationProvider",
+                                            "AuthProvider"],
+                exclude: [
+                    "Config",
+                    "Public",
+                    "Resources",
+                    ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,83 @@
+//
+//  UserController.swift
+//  hubIO-serverPackageDescription
+//
+//  Created by Kyle Lee on 1/21/18.
+//
+
+import Vapor
+
+internal struct UserController {
+    
+    private var drop: Droplet
+    
+    internal init(drop: Droplet) {
+        self.drop = drop
+    }
+    
+    internal func addRoutes() {
+
+        let group = drop.grouped("api", "users")
+        group.get(handler: getAllUsers)
+        group.get("usersCount", handler: getAllUsersCount)
+        group.post(handler: signUp)
+        group.post("signIn", handler: signIn)
+        
+    }
+    
+    private func getAllUsers(_ request: Request) throws -> ResponseRepresentable {
+        let users = try User.all()
+        return try users.makeJSON()
+    }
+    
+    private func getAllUsersCount(_ request: Request) throws -> ResponseRepresentable {
+        let users = try User.all()
+        return "users.count: \(users.count)"
+    }
+    
+    private func signUp(_ request: Request) throws -> ResponseRepresentable {
+        guard let json = request.json else { throw Abort.badRequest }
+        
+        let user = try User(json: json, drop: drop)
+        try user.save()
+        
+        let token = try AccessToken.generate(for: user)
+        print(token.token)
+        
+        return try user.makeJSON()
+    }
+    
+    private func signIn(_ request: Request) throws -> ResponseRepresentable {
+        guard let json = request.json else { throw Abort.badRequest }
+        
+        let username: String = try json.get(User.Keys.username)
+        let password: String = try json.get(User.Keys.password)
+        
+        let users = try User.makeQuery().filter(User.Keys.username, username)
+        
+        // FIND WAY TO MAKE SEARCH CASE INSENSITIVE
+        
+//        let users = try User.database?.raw("SELECT * FROM `users` WHERE username LIKE '\(username)'")
+        
+        guard
+            let user = try users.first(),
+            user.password == password
+            else { throw Abort(.badRequest, reason: "Hey dumbass, try again!") }
+        //invalid username or password
+        
+        return try user.makeJSON()
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
